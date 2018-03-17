6e2ac34ec75ab8b31d4485bb06a2c9add779e730
raj <241096raj@gmail.com>
2018-03-17 12:11:18 +0530

    Added XenPool to API feature

--- vendor/sircamp/xenapi/src/Sircamp/Xenapi/Xen.php
+++ vendor/sircamp/xenapi/src/Sircamp/Xenapi/Xen.php
@@ -5,6 +5,7 @@ use Sircamp\Xenapi\Element\XenVirtualMachine as  XenVirtualMachine;
 use Sircamp\Xenapi\Element\XenHost as XenHost;
 use Sircamp\Xenapi\Connection\XenConnection as  XenConnection;
 use Sircamp\Xenapi\Connection\XenResponse as  XenResponse;
+use Sircamp\Xenapi\Element\XenPool as XenPool;
 use Sircamp\Xenapi\Exception as  XenConnectionException;
 
 class Xen {
@@ -59,8 +60,52 @@ class Xen {
 		$response = new XenResponse($this->xenconnection->host__get_by_name_label($name));
 		return new XenHost($this->xenconnection,$name,$response->getValue()[0]);
 	}
+
+
+    /**
+     * get All records of all the pools known to system
+     *
+     * @param 
+     *
+     * @return mixed
+     */
+    public function getAllPools(){
+        $response = new XenResponse($this->xenconnection->pool__get_all_records());
+        $pool_array = $response->getValue();
+        $pools = array();	
+        foreach ($pool_array as $key => $value) {
+			$name = $value['name_label'];
+			$pool = new XenPool($this->xenconnection, $name, $key);
+			$pools = array_merge($pools, array($pool));     	
+        }
+        return $pools;      
+    }
+
+
+	public function getAllVMs(){
+		$response = new XenResponse($this->xenconnection->VM__get_all());
+        $VM_array = $response->getValue();
+        $VMs = array();	
+        foreach ($VM_array as $key => $value) {
+			
+			$VM = new XenVirtualMachine($this->xenconnection, (string)$key, $value);
+			$VMs = array_merge($VMs, array($VM));     	
+        }
+        return $VMs;
+    }
 	
 
+	public function getAllHosts(){
+		$response = new XenResponse($this->xenconnection->pool__get_all_records());
+        $pool_array = $response->getValue();
+        $pools = array();	
+        foreach ($pool_array as $key => $value) {
+			$name = $value['name_label'];
+			$pool = new XenPool($this->xenconnection, $name, $key);
+			$pools = array_merge($pools, array($pool));     	
+        }
+        return $pools;
+    }
 }
 
 ?>
